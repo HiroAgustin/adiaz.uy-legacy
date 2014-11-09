f272f7d1f97d2034be840e3b9f02c41aee8cfc91
HiroAgustin <hiroagustin@gmail.com>
2014-11-08 23:45:15 -0500

    chchchchch changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -182,7 +182,7 @@ module.exports = function (grunt)
       }
     , app: {
         src: ['<%= yeoman.app %>/**/*.html']
-      , ignorePath:  /\.\.\//
+      , ignorePath:  /\.\./
       }
     }
   , useminPrepare: {
